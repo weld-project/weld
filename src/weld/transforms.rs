b9d3d2872e520ccdf34c5c7804dbdaf52704c467
Matei Zaharia <matei@databricks.com>
2016-07-02 11:37:26 -0700

    Missing files

---
+++ src/weld/transforms.rs
@@ -0,0 +1,35 @@
+//! Common transformations on expressions.
+
+use super::ast::Expr;
+use super::ast::ExprKind::*;
+use super::error::*;
+
+/// Inlines Apply nodes whose argument is a Lambda expression. These often arise during macro
+/// expansion but it's simpler to inline them before doing type inference.
+/// Unlike many of the other transformations, we make this one independent of types so that
+/// we can apply it before type inference. 
+///
+/// Caveats:
+/// - Functions that reuse a parameter twice have its expansion appear twice, instead of assigning
+///   it to a temporary as would happen with function application.
+/// - Does not complete inlining if some of the functions take functions as arguments (in that
+///   case, the expressions after inlining may lead to more inlining).
+pub fn inline_apply<T:Clone>(expr: &mut Expr<T>) -> WeldResult<()> {
+    for child in expr.children_mut() {
+        try!(inline_apply(child));
+    }
+    let mut new_expr = None;
+    if let Apply(ref func, ref args) = expr.kind {
+        if let Lambda(ref params, ref body) = func.kind {
+            let mut new = *body.clone();
+            for (param, arg) in params.iter().zip(args) {
+                new.substitute(&param.name, &arg);
+            }
+            new_expr = Some(new);
+        }
+    }
+    if let Some(new) = new_expr {
+        *expr = new;
+    }
+    Ok(())
+}
