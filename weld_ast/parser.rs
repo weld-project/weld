99aab66fad4bbe3147a52b757ba657a0ff8aec42
Matei Zaharia <matei@databricks.com>
2016-08-28 13:22:38 -0700

    stuff

--- weld_ast/parser.rs
+++ weld_ast/parser.rs
@@ -1,9 +1,13 @@
 use std::vec::Vec;
 
 use weld_error::*;
+use super::ast::Symbol;
 use super::ast::BinOpKind::*;
 use super::ast::ExprKind::*;
+use super::ast::ScalarKind::*;
 use super::partial_types::*;
+use super::partial_types::PartialBuilderKind::*;
+use super::partial_types::PartialType::*;
 use super::tokenizer::*;
 use super::tokenizer::Token::*;
 
@@ -11,9 +15,16 @@ use super::tokenizer::Token::*;
 
 pub fn parse_expr(input: &str) -> WeldResult<PartialExpr> {
     let tokens = try!(tokenize(input));
-    Parser::new(&tokens).sum_expr().map(|b| *b)
+    Parser::new(&tokens).expr().map(|b| *b)
 }
 
+pub fn parse_type(input: &str) -> WeldResult<PartialType> {
+    let tokens = try!(tokenize(input));
+    Parser::new(&tokens).partial_type()
+}
+
+/// A stateful object that parses a sequence of tokens, tracking its position at each point.
+/// Assumes that the tokens end with a TEndOfInput.
 struct Parser<'t> {
     tokens: &'t [Token],
     position: usize
@@ -24,14 +35,44 @@ impl<'t> Parser<'t> {
         Parser { tokens: tokens, position: 0 }
     }
 
+    /// Look at the next token to be parsed.
     fn peek(&self) -> &'t Token {
         &self.tokens[self.position]
     }
 
+    /// Consume and return the next token. 
     fn next(&mut self) -> &'t Token {
         let token = &self.tokens[self.position];
         self.position += 1;
         token
+    }
+
+    /// Parse an expression starting at self.position.
+    fn expr(&mut self) -> WeldResult<Box<PartialExpr>> {
+        if *self.peek() == TLet {
+            self.let_expr()
+        } else {
+            self.sum_expr()
+        }
+    }
+
+    fn let_expr(&mut self) -> WeldResult<Box<PartialExpr>> {
+        if *self.next() != TLet {
+            return weld_err!("Expected 'let'");
+        }
+        let name = try!(self.name());
+        let ty = try!(self.optional_type());
+        if *self.next() != TEqual {
+            return weld_err!("Expected '='");
+        }
+        let value = try!(self.sum_expr());
+        if *self.next() != TSemicolon {
+            return weld_err!("Expected ';'");
+        }
+        let body = try!(self.expr());
+        let mut expr = expr_box(Let(name, value, body));
+        expr.ty = ty;
+        Ok(expr)
     }
 
     fn sum_expr(&mut self) -> WeldResult<Box<PartialExpr>> {
@@ -49,10 +90,10 @@ impl<'t> Parser<'t> {
     }
 
     fn prod_expr(&mut self) -> WeldResult<Box<PartialExpr>> {
-        let mut res = try!(self.terminal());
+        let mut res = try!(self.leaf_expr());
         while *self.peek() == TTimes || *self.peek() == TDivide {
             let token = self.next();
-            let right = try!(self.terminal());
+            let right = try!(self.leaf_expr());
             if *token == TTimes {
                 res = expr_box(BinOp(Multiply, res, right))
             } else {
@@ -62,13 +103,14 @@ impl<'t> Parser<'t> {
         Ok(res)
     }
 
-    fn terminal(&mut self) -> WeldResult<Box<PartialExpr>> {
+    fn leaf_expr(&mut self) -> WeldResult<Box<PartialExpr>> {
         match *self.next() {
-            TI32Lit(value) => Ok(expr_box(I32Literal(value))),
+            TI32Literal(value) => Ok(expr_box(I32Literal(value))),
+            TBoolLiteral(value) => Ok(expr_box(BoolLiteral(value))),
             TIdent(ref name) => Ok(expr_box(Ident(name.clone()))),
 
             TOpenParen => {
-                let expr = try!(self.sum_expr());
+                let expr = try!(self.expr());
                 if *self.next() != TCloseParen {
                     return weld_err!("Expected ')'")
                 }
@@ -78,7 +120,7 @@ impl<'t> Parser<'t> {
             TOpenBracket => {
                 let mut exprs: Vec<PartialExpr> = Vec::new();
                 while *self.peek() != TCloseBracket {
-                    let expr = try!(self.sum_expr());
+                    let expr = try!(self.expr());
                     exprs.push(*expr);
                     if *self.peek() == TComma {
                         self.next();
@@ -90,7 +132,95 @@ impl<'t> Parser<'t> {
                 Ok(expr_box(MakeVector(exprs)))
             }
 
-            ref other => weld_err!("Expected literal but got {:?}", other)
+            TOpenBrace => {
+                let mut exprs: Vec<PartialExpr> = Vec::new();
+                while *self.peek() != TCloseBrace {
+                    let expr = try!(self.expr());
+                    exprs.push(*expr);
+                    if *self.peek() == TComma {
+                        self.next();
+                    } else if *self.peek() != TCloseBrace {
+                        return weld_err!("Expected ',' or '}}'")
+                    }
+                }
+                self.next();  // Will be a TCloseBrace
+                Ok(expr_box(MakeStruct(exprs)))
+            }
+
+            ref other => weld_err!("Expected expression but got '{}'", other)
+        }
+    }
+
+    fn name(&mut self) -> WeldResult<Symbol> {
+        match *self.next() {
+            TIdent(ref name) => Ok(name.clone()),
+            ref other => weld_err!("Expected identifier but got '{}'", other)
+        }
+    }
+
+    fn optional_type(&mut self) -> WeldResult<PartialType> {
+        if *self.peek() == TColon {
+            self.next();   // Skip TColon
+            self.partial_type() 
+        } else {
+            Ok(Unknown)
+        }
+    }
+
+    fn partial_type(&mut self) -> WeldResult<PartialType> {
+        match *self.next() {
+            TQuestion => Ok(Unknown),
+
+            TIdent(ref name) => {
+                match name.as_ref() {
+                    "i32" => Ok(Scalar(I32)),
+                    "i64" => Ok(Scalar(I64)),
+                    "f32" => Ok(Scalar(F32)),
+                    "f64" => Ok(Scalar(F64)),
+                    "bool" => Ok(Scalar(Bool)),
+
+                    "vec" => {
+                        if *self.next() != TOpenBracket {
+                            return weld_err!("Expected '['");
+                        }
+                        let elem_type = try!(self.partial_type());
+                        if *self.next() != TCloseBracket {
+                            return weld_err!("Expected ']'");
+                        }
+                        Ok(Vector(Box::new(elem_type)))
+                    }
+
+                    "appender" => {
+                        if *self.next() == TOpenBracket {
+                            return weld_err!("Expected '['");
+                        }
+                        let elem_type = try!(self.partial_type());
+                        if *self.next() != TCloseBracket {
+                            return weld_err!("Expected ']'");
+                        }
+                        Ok(Builder(Appender(Box::new(elem_type))))
+                    }
+
+                    other => weld_err!("Expected type but got '{}'", other)
+                }
+            },
+
+            TOpenBrace => {
+                let mut types: Vec<PartialType> = Vec::new();
+                while *self.peek() != TCloseBrace {
+                    let ty = try!(self.partial_type());
+                    types.push(ty);
+                    if *self.peek() == TComma {
+                        self.next();
+                    } else if *self.peek() != TCloseBrace {
+                        return weld_err!("Expected ',' or '}}'")
+                    }
+                }
+                self.next();  // Will be a TCloseBrace
+                Ok(Struct(types))
+            }
+
+            ref other => weld_err!("Expected type but got '{}'", other)
         }
     }
 }
@@ -106,5 +236,14 @@ fn basic_parsing() {
     let e = parse_expr("[1, 2+3, 2]").unwrap();
     assert_eq!(print_expr(&e), "[1,(2+3),2]");
 
+    let e = parse_expr("let a = 3+2; let b = (let c=a; c); b").unwrap();
+    assert_eq!(print_expr(&e), "let a=((3+2));let b=(let c=(a);c);b");
+
+    let e = parse_expr("let a: vec[i32] = [2, 3]; a").unwrap();
+    assert_eq!(print_expr(&e), "let a=([2,3]);a");
+
     assert!(parse_expr("10 * * 2").is_err());
+
+    let t = parse_type("{i32, vec[vec[?]], ?}").unwrap();
+    assert_eq!(print_type(&t), "{i32,vec[vec[?]],?}");
 }
--- weld_ast/tokenizer.rs
+++ weld_ast/tokenizer.rs
@@ -1,16 +1,20 @@
+use std::fmt;
 use std::str::FromStr;
 use std::vec::Vec;
+
 use regex::Regex;
 
 use weld_error::*;
 
 #[derive(Clone,Debug,PartialEq)]
 pub enum Token {
-    TI32Lit(i32),
+    TI32Literal(i32),
+    TBoolLiteral(bool),
     TIdent(String),
     TIf,
     TFor,
     TLet,
+    TMacro,
     TOpenParen,     // (
     TCloseParen,    // )
     TOpenBracket,   // [
@@ -22,7 +26,12 @@ pub enum Token {
     TMinus,
     TTimes,
     TDivide,
+    TEqual,
     TBar,           // |
+    TDot,
+    TColon,
+    TSemicolon,
+    TQuestion,
     TEndOfInput
 }
 
@@ -30,10 +39,11 @@ pub enum Token {
 pub fn tokenize(input: &str) -> WeldResult<Vec<Token>> {
     lazy_static! {
         // Regular expression for splitting up tokens.
-        static ref TOKEN_RE: Regex = Regex::new(r"[A_Za-z0-9_.]+|[-+/*,()[\]]|\S+").unwrap();
+        static ref TOKEN_RE: Regex = Regex::new(
+            r"[A_Za-z0-9_.]+|[-+/*,=()[\]{}|&.:;?]|\S+").unwrap();
 
         // Regular expressions for various types of tokens. 
-        static ref KEYWORD_RE: Regex = Regex::new(r"if|for|let").unwrap();
+        static ref KEYWORD_RE: Regex = Regex::new(r"if|for|let|true|false|macro").unwrap();
         static ref IDENT_RE: Regex = Regex::new(r"^[A_Za-z_][A_Za-z0-9_]*$").unwrap();
         static ref I32_RE: Regex = Regex::new(r"^[0-9]+$").unwrap();
     }
@@ -49,13 +59,16 @@ pub fn tokenize(input: &str) -> WeldResult<Vec<Token>> {
                 "if" => TIf,
                 "let" => TLet,
                 "for" => TFor,
+                "macro" => TMacro,
+                "true" => TBoolLiteral(true),
+                "false" => TBoolLiteral(false),
                 _ => return weld_err!("Invalid input token: {}", text)
             });
         } else if IDENT_RE.is_match(text) {
             tokens.push(TIdent(text.to_string()));
         } else if I32_RE.is_match(text) {
             match i32::from_str(text) {
-                Ok(value) => tokens.push(TI32Lit(value)),
+                Ok(value) => tokens.push(TI32Literal(value)),
                 Err(_) => return weld_err!("Invalid i32 literal: {}", text)
             }
         } else {
@@ -72,6 +85,11 @@ pub fn tokenize(input: &str) -> WeldResult<Vec<Token>> {
                 "}" => TCloseBrace,
                 "|" => TBar,
                 "," => TComma,
+                "=" => TEqual,
+                "." => TDot,
+                ":" => TColon,
+                ";" => TSemicolon,
+                "?" => TQuestion,
                 _ => return weld_err!("Invalid input token: {}", text)
             });
         }
@@ -82,12 +100,50 @@ pub fn tokenize(input: &str) -> WeldResult<Vec<Token>> {
     return Ok(tokens);
 }
 
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::Token::*;
+        match *self {
+            TI32Literal(ref value) => write!(f, "{}", value),
+            TBoolLiteral(ref value) => write!(f, "{}", value),
+            TIdent(ref value) => write!(f, "{}", value),
+            ref other => write!(f, "{}", match *other {
+                TI32Literal(_) => "",
+                TBoolLiteral(_) => "",
+                TIdent(_) => "",
+                TIf => "if",
+                TFor => "for",
+                TLet => "let",
+                TMacro => "macro",
+                TOpenParen => "(",
+                TCloseParen => ")",
+                TOpenBracket => "[",
+                TCloseBracket => "]",
+                TOpenBrace => "{",
+                TCloseBrace => "}",
+                TComma => ",",
+                TPlus => "+",
+                TMinus => "-",
+                TTimes => "*",
+                TDivide => "/",
+                TEqual => "=",
+                TBar => "|",
+                TDot => ".",
+                TColon => ":",
+                TSemicolon => ";",
+                TQuestion => "?",
+                TEndOfInput => "<END>"
+            })
+        }
+    }
+}
+
 #[test]
 fn basic_tokenize() {
     use self::Token::*;
 
     assert_eq!(tokenize("a for 23 + z0").unwrap(),
-        vec![TIdent("a".into()), TFor, TI32Lit(23), TPlus, TIdent("z0".into()), TEndOfInput]);
+        vec![TIdent("a".into()), TFor, TI32Literal(23), TPlus, TIdent("z0".into()), TEndOfInput]);
 
     assert!(tokenize("0a").is_err());
     assert!(tokenize("#").is_err());
