611e56960f654df666d8244eac48c5030a091338
Shoumik Palkar <shoumikpalkar@gmail.com>
2017-01-29 20:05:22 -0800

    First cut C API (#35)
    Implements a C API for compiling and running Weld modules. Also includes samples in Python (using ctypes) and C++ for how the library is used.

---
+++ api/examples/cpp/add_repl/add_repl.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <string.h>
+
+// Include the Weld API.
+#include "../../../weld.h"
+
+int main() {
+    // Compile Weld module.
+    weld_error_t e = NULL;
+    weld_module_t m = weld_module_compile("|x:i64| x+5L", "configuration", &e);
+
+    if (weld_error_code(e)) {
+        const char *err = weld_error_message(e);
+        printf("Error message: %s\n", err);
+    }
+
+    // Create a Weld Object for the argument.
+    
+    while(true) {
+
+        char buf[4096];
+        char *c;
+        printf(">>> ");
+        fgets(buf, sizeof(buf), stdin);
+
+        if (strncmp(buf, "quit", sizeof(buf)) == 0) {
+            break;
+        }
+
+        unsigned long x = strtoul(buf, &c, 10);
+        if (c == buf) {
+            printf("nope, try again.\n");
+            continue;
+        }
+
+        int64_t input = (int64_t)x;
+        weld_value_t arg = weld_value_new(&input);
+
+        // Run the module and get the result.
+        weld_value_t result = weld_module_run(m, arg, &e);
+        void *result_data = weld_value_data(result);
+        printf("Answer: %lld\n", *(int64_t *)result_data);
+
+        // Free the values.
+        weld_value_free(result);
+        weld_value_free(arg);
+    }
+
+    weld_error_free(e);
+    weld_module_free(m);
+    printf("Freeing data and quiting!\n");
+
+    return 0;
+}
