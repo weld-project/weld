f658edb1459e647ea173f3ab274e1a3ad534df24
Shoumik Palkar <shoumik@cs.stanford.edu>
2017-02-23 00:30:12 -0800

    Simplify benchmarking code

--- benches/benches.rs
+++ benches/benches.rs
@@ -15,7 +15,7 @@ use self::weld::weld_print_function_pointers;
 use self::weld::WeldRuntimeErrno;
 
 use self::weld::{weld_conf_new, weld_conf_set, weld_conf_free};
-use self::weld::{weld_error_code, weld_error_message, weld_error_free};
+use self::weld::{weld_error_code, weld_error_free};
 use self::weld::{weld_value_new, weld_value_data, weld_value_free};
 use self::weld::{weld_module_compile, weld_module_run, weld_module_free};
 use self::weld::{WeldModule, WeldValue, WeldConf, WeldError};
@@ -47,6 +47,7 @@ fn benchmark_conf() -> *mut WeldConf {
     conf
 }
 
+/// Returns a compiled, runnable Weld module.
 unsafe fn compile_program(code: &str) -> Result<*mut WeldModule, ()> {
     let code = CString::new(code).unwrap();
     let conf = benchmark_conf();
@@ -64,6 +65,8 @@ unsafe fn compile_program(code: &str) -> Result<*mut WeldModule, ()> {
     Ok(module)
 }
 
+/// Runs a `WeldModule` and returns either a `WeldValue` or `WeldError`, depending on whether the
+/// run was successful.
 unsafe fn run_module<T>(module: *mut WeldModule,
                         ptr: &T)
                         -> Result<*mut WeldValue, *mut WeldError> {
@@ -100,8 +103,7 @@ fn bench_vector_sum(bench: &mut Bencher) {
         y: WeldVec<i32>,
     }
 
-    let code = "|x:vec[i32],y:vec[i32]| map(zip(x,y), |e| e.$0 + e.$1)";
-    let conf = benchmark_conf();
+    let code = include_str!("benchmarks/vector_sum.weld");
 
     // 100MB of data
     let data_size: usize = 2 << 26;
@@ -145,12 +147,8 @@ fn bench_vector_sum(bench: &mut Bencher) {
     unsafe { weld_module_free(module) };
 }
 
-fn bench_integer_map_reduce(bench: &mut Bencher) {
-
-    let code = "|x:vec[i32]| result(for(map(x, |e| e * 4), merger[i32,+], |b,i,e| \
- merge(b, e)))";
-
-    println!("{}", code);
+fn bench_map_reduce(bench: &mut Bencher) {
+    let code = include_str!("benchmarks/map_reduce.weld");
 
     // 100MB of data
     let data_size: usize = 2 << 26;
@@ -182,64 +180,159 @@ fn bench_integer_map_reduce(bench: &mut Bencher) {
     });
 }
 
-// fn bench_tpch_q6(bench: &mut Bencher) {
-// let code = include_str!("benchmarks/tpch/q6.weld");
-//
-// #[allow(dead_code)]
-// struct Args {
-// l_shipdate: WeldVec,
-// l_discount: WeldVec,
-// l_quantity: WeldVec,
-// l_ep: WeldVec,
-// }
-//
-// Setup the arguments.
-// let size: i64 = 500;
-// let l_shipdate: Vec<i32> = vec![19940505; size as usize];
-// let l_discount: Vec<i32> = vec![6; size as usize];
-// let l_quantity: Vec<i32> = vec![20; size as usize];
-// let l_ep: Vec<i32> = vec![100; size as usize];
-//
-// All predicates pass
-// let expect = l_ep[0] * l_discount[0] * (size as i32);
-//
-// let args = Args {
-// l_shipdate: WeldVec {
-// data: l_shipdate.as_ptr() as *const i32,
-// len: size,
-// },
-// l_discount: WeldVec {
-// data: l_discount.as_ptr() as *const i32,
-// len: size,
-// },
-// l_quantity: WeldVec {
-// data: l_quantity.as_ptr() as *const i32,
-// len: size,
-// },
-// l_ep: WeldVec {
-// data: l_ep.as_ptr() as *const i32,
-// len: size,
-// },
-// };
-//
-// let inp = Box::new(llvm::WeldInputArgs {
-// input: &args as *const Args as i64,
-// nworkers: 1,
-// run_id: 0,
-// });
-// let ptr = Box::into_raw(inp) as i64;
-//
-// let module = compile(code);
-// let result_raw = module.run(ptr) as *const i32;
-// let result = unsafe { *result_raw.clone() };
-// assert_eq!(result, expect as i32);
-//
-// bench.iter(|| {
-// module.run(ptr) as *const i32;
-// weld_run_free(-1);
-// })
-// }
-//
+fn bench_tpch_q1(bench: &mut Bencher) {
+    let code = include_str!("benchmarks/tpch/q1.weld");
+
+    #[allow(dead_code)]
+    struct Args {
+        l_returnflag: WeldVec<i8>,
+        l_linestatus: WeldVec<i8>,
+        l_quantity: WeldVec<f32>,
+        l_ep: WeldVec<f32>,
+        l_discount: WeldVec<f32>,
+        l_shipdate: WeldVec<i32>,
+        l_tax: WeldVec<f32>,
+    }
+
+    struct ReturnValue {
+        l_returnflag: i8,
+        l_linestatus: i8,
+        sum_qty: f32,
+        sum_base_price: f32,
+        sum_disc_price: f32,
+        sum_charge: f32,
+        avg_qty: f32,
+        avg_price: f32,
+        count_order: i32,
+    }
+
+    // 100MB of data per column.
+    let data_size: usize = 2 << 26;
+    let size: usize = data_size / std::mem::size_of::<i32>();
+
+    // Set values so all predicates pass.
+    let l_returnflag: Vec<i8> = vec![1; size ];
+    let l_linestatus: Vec<i8> = vec![1; size ];
+    let l_quantity: Vec<f32> = vec![20.0; size ];
+    let l_ep: Vec<f32> = vec![100.0; size];
+    let l_discount: Vec<f32> = vec![6.0; size ];
+    let l_shipdate: Vec<i32> = vec![19940505; size ];
+    let l_tax: Vec<f32> = vec![6.0; size ];
+
+
+    let ref args = Args {
+        l_returnflag: WeldVec {
+            data: l_returnflag.as_ptr() as *const i8,
+            len: size as i64,
+        },
+        l_linestatus: WeldVec {
+            data: l_linestatus.as_ptr() as *const i8,
+            len: size as i64,
+        },
+        l_quantity: WeldVec {
+            data: l_quantity.as_ptr() as *const f32,
+            len: size as i64,
+        },
+        l_ep: WeldVec {
+            data: l_ep.as_ptr() as *const f32,
+            len: size as i64,
+        },
+        l_discount: WeldVec {
+            data: l_discount.as_ptr() as *const f32,
+            len: size as i64,
+        },
+        l_shipdate: WeldVec {
+            data: l_shipdate.as_ptr() as *const i32,
+            len: size as i64,
+        },
+        l_tax: WeldVec {
+            data: l_tax.as_ptr() as *const f32,
+            len: size as i64,
+        },
+    };
+
+    let module = unsafe { compile_program(code).unwrap() };
+
+    // Run once to check correctness/warm up.
+    let ret_value = unsafe { run_module(module, args).unwrap() };
+    let data = unsafe { weld_value_data(ret_value) as *const i32 };
+    let result = unsafe { *data };
+
+    // TODO(shoumik)
+    // Since all predicates pass, this is the result
+    // let expect = 0;
+    // assert_eq!(result, expect);
+    unsafe { weld_value_free(ret_value) };
+
+    bench.iter(|| {
+        match unsafe { run_module(module, args) } {
+            Ok(v) => unsafe { weld_value_free(v) },
+            Err(e) => unsafe { weld_error_free(e) },
+        }
+    });
+}
+
+fn bench_tpch_q6(bench: &mut Bencher) {
+    let code = include_str!("benchmarks/tpch/q6.weld");
+
+    #[allow(dead_code)]
+    struct Args {
+        l_shipdate: WeldVec<i32>,
+        l_discount: WeldVec<f32>,
+        l_quantity: WeldVec<f32>,
+        l_ep: WeldVec<f32>,
+    }
+
+    // 100MB of data per column.
+    let data_size: usize = 2 << 26;
+    let size: usize = data_size / std::mem::size_of::<i32>();
+
+    // Set values so all predicates pass.
+    let l_shipdate: Vec<i32> = vec![19940505; size ];
+    let l_discount: Vec<f32> = vec![6.0; size ];
+    let l_quantity: Vec<f32> = vec![20.0; size ];
+    let l_ep: Vec<f32> = vec![100.0; size];
+
+    // Since all predicates pass, this is the result
+    let expect = l_ep[0] * l_discount[0] * (size as f32);
+
+    let ref args = Args {
+        l_shipdate: WeldVec {
+            data: l_shipdate.as_ptr() as *const i32,
+            len: size as i64,
+        },
+        l_discount: WeldVec {
+            data: l_discount.as_ptr() as *const f32,
+            len: size as i64,
+        },
+        l_quantity: WeldVec {
+            data: l_quantity.as_ptr() as *const f32,
+            len: size as i64,
+        },
+        l_ep: WeldVec {
+            data: l_ep.as_ptr() as *const f32,
+            len: size as i64,
+        },
+    };
+
+    let module = unsafe { compile_program(code).unwrap() };
+
+    // Run once to check correctness/warm up.
+    let ret_value = unsafe { run_module(module, args).unwrap() };
+    let data = unsafe { weld_value_data(ret_value) as *const f32 };
+    let result = unsafe { *data };
+
+    assert_eq!(result, expect);
+    unsafe { weld_value_free(ret_value) };
+
+    bench.iter(|| {
+        match unsafe { run_module(module, args) } {
+            Ok(v) => unsafe { weld_value_free(v) },
+            Err(e) => unsafe { weld_error_free(e) },
+        }
+    });
+}
+
 
 
 /// Register functions that can be run with the benchmarking suite here.
@@ -249,9 +342,8 @@ pub fn registered_benchmarks() -> HashMap<String, fn(&mut bencher::Bencher)> {
 
     let mut benchmarks_all: HashMap<String, fn(&mut bencher::Bencher)> = HashMap::new();
     benchmarks_all.insert("bench_vector_sum".to_string(), bench_vector_sum);
-    benchmarks_all.insert("bench_integer_map_reduce".to_string(),
-                          bench_integer_map_reduce);
+    benchmarks_all.insert("bench_map_reduce".to_string(), bench_map_reduce);
+    // benchmarks_all.insert("bench_tpch_q1".to_string(), bench_tpch_q1);
     // benchmarks_all.insert("bench_tpch_q6".to_string(), bench_tpch_q6);
-    //
     benchmarks_all
 }
