16603c99355dd44b9a9059a9e7a9b4bde08b91ae
Shoumik Palkar <shoumik@cs.stanford.edu>
2017-06-01 14:00:52 -0700

    Delete comments

--- weld/vectorizer.rs
+++ weld/vectorizer.rs
@@ -46,27 +46,6 @@ fn vectorizable_iters(iters: &Vec<Iter<Type>>) -> bool {
 
 /// Vectorize an expression.
 pub fn vectorize(expr: &mut Expr<Type>) -> WeldResult<()> {
-    // Step 1. Check each loop to see if it can be vectorized.
-    // The complex case here is nested loops; I'll implement simple loops with fringing first.
-    //
-    // How can loops be "partially" vectorized? We can break a loop into multiple parts, or blocks.
-    // There are two challenges with this:
-    // 1) We need a node to pull out individual vector elements and then perform operations over
-    //    them.
-    // 2) We need to determine when to compose these elements back into a vector once this is done.
-    //
-    // For example, a loop which is vectorizable but has a single dictionary lookup can unravel the
-    // unvectorizable part.
-    //
-    // Some cases to consider:
-    //
-    // B = merger[+]
-    // for x in V:
-    //      merge(B, x)
-    //
-    // B = appender
-    // for x in V
-    //      merge(B, x)
     expr.transform_and_continue(&mut |ref mut expr| {
         //  The Res is a stricter-than-necessary check, but prevents us from having to check nested
         //  loops for now.
