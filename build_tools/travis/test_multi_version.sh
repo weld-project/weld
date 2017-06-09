38d74c140134928cb4319f835bb4657df7ba2bfb
Jacob Scott <jhscott@infer.com>
2017-06-09 16:59:08 -0700

    Incorporate PR feedback

--- build_tools/travis/test_multi_version.sh
+++ build_tools/travis/test_multi_version.sh
@@ -3,14 +3,14 @@
 LLVM_VERSION=$1
 LLVM_SYS_VERSION=$2
 PYTHON_VERSION=$3
+VENV_HOME=`pwd`/.virtualenv
 
 # create llvm-config symlink
-sudo rm -f `which llvm-config`
 sudo rm -f /usr/bin/llvm-config
 sudo ln -s /usr/bin/llvm-config-$LLVM_VERSION /usr/bin/llvm-config
 export WELD_HOME=`pwd`
 
-source $VENV_HOME/py$PYTHON_VERSION/bin/activate
+source $VENV_HOME/python$PYTHON_VERSION/bin/activate
 cd python
 python setup.py install
 cd ..
@@ -27,3 +27,4 @@ cargo test
 
 python python/grizzly/tests/grizzly_test.py
 python python/grizzly/tests/numpy_weld_test.py
+deactivate
