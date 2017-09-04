4b2dbe3e193581766a93b7e45fae352074718430
Matei Zaharia <matei.zaharia@gmail.com>
2017-09-03 18:19:14 -0700

    fix

--- weld/easy_ll/tests.rs
+++ weld/easy_ll/tests.rs
@@ -15,7 +15,7 @@ fn basic_use() {
            %2 = call i64 @bar(i64 %1)
            ret i64 %2
        }
-    ", None);
+    ", 2, None);
     assert!(module.is_ok());
     assert_eq!(module.unwrap().run(42), 44);
 }
@@ -26,7 +26,7 @@ fn compile_error() {
        define ZZZZZZZZ @run(i64 %arg) {
            ret i64 0
        }
-    ", None);
+    ", 2, None);
     assert!(!module.is_ok());
     assert!(module.unwrap_err().description().contains("Compile"));
 }
@@ -37,7 +37,7 @@ fn no_run_function() {
        define i64 @ZZZZZZZ(i64 %arg) {
            ret i64 0
        }
-    ", None);
+    ", 2, None);
     assert!(!module.is_ok());
     assert!(module.unwrap_err().description().contains("run function"));
 }
@@ -48,7 +48,7 @@ fn wrong_function_type() {
        define i64 @run() {
            ret i64 0
        }
-    ", None);
+    ", 2, None);
     assert!(!module.is_ok());
     assert!(module.unwrap_err().description().contains("wrong type"));
 }
