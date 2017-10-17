5da9969216d851cde257777a5f8e03994ff27116
rahul palamuttam <rahulpalamut@gmail.com>
2017-10-17 16:15:25 -0700

    Build refactor (#240)

--- python/grizzly/setup.py
+++ python/grizzly/setup.py
@@ -7,6 +7,7 @@ import sys
 from setuptools import setup, Distribution
 import setuptools.command.build_ext as _build_ext
 from setuptools.command.install import install
+from setuptools.command.develop import develop
 
 system = platform.system()
 if system == 'Linux':
@@ -22,19 +23,35 @@ class Install(install):
     def run(self):
         install.run(self)
         python_executable = sys.executable
-        protoc_command = ["make -C " + self.install_lib + "grizzly/ EXEC=" + python_executable]
-        if subprocess.call(protoc_command, shell=True) != 0:
+        protoc_command_clean = ["make -C " + self.install_lib + "grizzly clean"]
+        if subprocess.call(protoc_command_clean, shell=True) != 0:
+            sys.exit(-1)
+
+        protoc_command_make = ["make -C " + self.install_lib + "grizzly/ EXEC=" + python_executable]
+        if subprocess.call(protoc_command_make, shell=True) != 0:
+            sys.exit(-1)
+
+class Develop(develop):
+    def run(self):
+        develop.run(self)
+        python_executable = sys.executable
+        protoc_command_clean = ["make -C grizzly/ clean"]
+        if subprocess.call(protoc_command_clean, shell=True) != 0:
+            sys.exit(-1)
+
+        protoc_command_make = ["make -C grizzly/ EXEC=" + python_executable]
+        if subprocess.call(protoc_command_make, shell=True) != 0:
             sys.exit(-1)
 
 class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return True
 
-setup(name='grizzly',
+setup(name='pygrizzly',
       version='0.0.1',
       packages=['grizzly'],
       package_data = {'grizzly': ['numpy_weld_convertor.cpp', 'common.h', 'Makefile']},
-      cmdclass={"install": Install},
+      cmdclass={"install": Install, "develop": Develop},
       distclass=BinaryDistribution,
       url='https://github.com/weld-project/weld',
       author='Weld Developers',
