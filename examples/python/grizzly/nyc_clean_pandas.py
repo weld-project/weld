4c42ce09eba1e788ddc98f010e48f93ca45d8687
Shoumik Palkar <shoumik@cs.stanford.edu>
2017-12-03 18:58:09 -0800

    More efficient pandas implementation of NYC

---
+++ examples/python/grizzly/nyc_clean_pandas.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+"""
+This workload is adapted from http://rawgit.com/vberaudi/utwt/master/nyc_taxis.html.
+Data: `wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2014-01.csv`
+(Newer data may have different format).
+"""
+
+import tempfile
+import os
+
+import argparse
+import time
+
+import pandas as pd
+import grizzly.grizzly as gr
+
+# Default CSV
+CSV_DEFAULT = "data/yellow_tripdata_2014-01.csv"
+# Use Pandas by default.
+GRIZZLY_DEFAULT = False
+
+def run(filename, use_grizzly, passes=None, threads=1):
+    """
+    Loads data, prints the number of initial rows, and wraps the DataFrame
+    as a DataFrameWeld if use_grizzly is True.
+    Returns the DataFrame and the initial size of the DataFrame.
+    """
+    ft = pd.read_csv(filename)
+    databegin = len(ft)
+    print("We have " + str(databegin) + " trips in New York in January")
+
+    # Clean up the columns.
+    cols = [c.strip() for c in list(ft.columns.values)]
+    ft.columns = cols
+    mycols = {'pickup_datetime', 'dropoff_datetime', 'passenger_count',
+            'trip_time_in_secs', 'trip_distance', 'pickup_longitude',
+            'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude'}
+
+    for c in cols:
+        if c not in mycols:
+            ft.drop(c, 1)
+
+    if use_grizzly:
+        ft = gr.DataFrameWeld(ft)
+
+    print "Starting timed run..."
+    start = time.time()
+
+    ft = ft[((ft['trip_distance'] != 0) &
+            (ft['total_amount'] != 0) &
+            (ft['pickup_longitude'] < -73) &
+            (ft['pickup_longitude'] > -76) &
+            (ft['pickup_latitude'] > 39) &
+            (ft['pickup_latitude'] < 43) &
+            (ft['dropoff_longitude'] < -73) &
+            (ft['dropoff_longitude'] > -76) &
+            (ft['dropoff_latitude'] > 39) &
+            (ft['dropoff_latitude'] < 43) &
+            (ft['passenger_count'] <= 3) &
+            (ft['trip_distance'] != 0))]
+
+    if use_grizzly:
+        print "Evaluating Length with Grizzly..."
+        length = ft.len(passes=passes, threads=threads)
+    else:
+        length = len(ft)
+
+    end = time.time()
+
+    print "Length:", length
+
+    print "Removed " + str(databegin - length) + " bad records."
+
+    print "{} seconds".format(end - start)
+
+    return end - start
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Clean the NYC Taxi Cab dataset.")
+
+    parser.add_argument('-f', '--filename', type=str, default=CSV_DEFAULT)
+    parser.add_argument('-p', '--passes', type=str, default=None)
+    parser.add_argument('-t', '--threads', type=int, default=1)
+
+    args = parser.parse_args()
+
+    print "File={} Grizzly={} Threads={}".format(args.filename, True, args.threads)
+
+    if args.passes is not None:
+        passes = [p.strip().lower() for p in args.passes.split(",")]
+        print "Passes:", passes
+    else:
+        passes = None
+
+    time = run(args.filename, False, passes=passes, threads=str(args.threads))
+
+
