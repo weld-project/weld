9af7cf10547f5d86bd2ef11f33f39b6964d0e5a7
Parimarjan Negi <pnegi@stanford.edu>
2018-01-22 04:10:56 -0800

    running nditer tests from travis
    * these tests might be redundant after we add NumPy tests too - so not
    sure if we should be running them?

--- build_tools/travis/test_multi_version.sh
+++ build_tools/travis/test_multi_version.sh
@@ -34,6 +34,9 @@ export LD_LIBRARY_PATH=`pwd`/target/release
 python python/grizzly/tests/grizzly_test.py
 python python/grizzly/tests/numpy_weld_test.py
 
+# run tests for nditer
+python examples/python/nditer/nditer_test.py
+
 cd $WELD_HOME/weld-benchmarks; python run_benchmarks.py -b tpch_q1 tpch_q6 vector_sum map_reduce data_cleaning crime_index crime_index_simplified -n 5 -f results.tsv -v -d -p performance.png
 mkdir -p $WELD_HOME/results
 mv performance.png $WELD_HOME/results
