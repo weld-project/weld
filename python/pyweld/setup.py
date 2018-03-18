c2c49b2ddf1019eb934881318825764f33fe7d67
Deepak Narayanan <deepakn94@gmail.com>
2018-03-18 09:10:25 +0000

    udpate version one last time

--- python/pyweld/setup.py
+++ python/pyweld/setup.py
@@ -59,7 +59,7 @@ class BinaryDistribution(Distribution):
         return True
 
 setup(name='pyweld',
-      version='0.0.3',
+      version='0.0.4',
       packages=['weld'],
       cmdclass={"build_ext": build_ext, "develop": Develop},
       distclass=BinaryDistribution,
