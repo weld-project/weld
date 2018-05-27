371bdab9e9468ca9af4ab3053cad3746d029f831
Shoumik Palkar <shoumik@cs.stanford.edu>
2018-05-27 00:03:07 -0700

    yet more type inference cleanup

--- weld/new_types/inference.rs
+++ weld/new_types/inference.rs
@@ -3,6 +3,9 @@
 extern crate fnv;
 
 use new_types::*;
+use new_types::LiteralKind::*;
+use new_types::BuilderKind::*;
+use new_types::ScalarKind::*;
 
 use fnv::FnvHashMap;
 
@@ -46,12 +49,12 @@ trait PushType {
     /// Sets this `Type` to be `other`.
     ///
     /// This function returns an error if `other` is incompatible with this `Type`.
-    fn set(&mut self, other: Self) -> WeldResult<bool>;
+    fn push_complete(&mut self, other: Self) -> WeldResult<bool>;
 }
 
 impl PushType for Type {
-
-    fn set(&mut self, other: Type) -> WeldResult<bool> {
+    /// Sets this `Type` to be `other`.
+    fn push_complete(&mut self, other: Type) -> WeldResult<bool> {
         match other {
             Scalar(_) | Simd(_) if *self == Unknown => {
                 *self = other;
@@ -66,7 +69,7 @@ impl PushType for Type {
             }
             Vector(ref elem) => {
                 if let Vector(ref mut dest) = *self {
-                    dest.set(elem.as_ref().clone())
+                    dest.push_complete(elem.as_ref().clone())
                 } else {
                     compile_err!("blah")
                 }
@@ -173,7 +176,6 @@ trait InferTypesInternal {
     fn infer_up(&mut self, &mut TypeMap) -> WeldResult<bool>;
 }
 
-
 impl InferTypesInternal for Expr {
     /// Internal implementation of type inference.
     fn infer_types_internal(&mut self) -> WeldResult<()> {
@@ -231,6 +233,310 @@ impl InferTypesInternal for Expr {
     }
 
     fn infer_locally(&mut self, env: &TypeMap) -> WeldResult<bool> {
-        Ok(true)
+        match self.kind {
+            // Literal values - force the type to match the literal.
+            Literal(I8Literal(_)) => self.ty.push_complete(Scalar(I8)),
+            Literal(I16Literal(_)) => self.ty.push_complete(Scalar(I16)),
+            Literal(I32Literal(_)) => self.ty.push_complete(Scalar(I32)),
+            Literal(I64Literal(_)) => self.ty.push_complete(Scalar(I64)),
+            Literal(U8Literal(_)) => self.ty.push_complete(Scalar(U8)),
+            Literal(U16Literal(_)) => self.ty.push_complete(Scalar(U16)),
+            Literal(U32Literal(_)) => self.ty.push_complete(Scalar(U32)),
+            Literal(U64Literal(_)) => self.ty.push_complete(Scalar(U64)),
+            Literal(F32Literal(_)) => self.ty.push_complete(Scalar(F32)),
+            Literal(F64Literal(_)) => self.ty.push_complete(Scalar(F64)),
+            Literal(BoolLiteral(_)) => self.ty.push_complete(Scalar(Bool)),
+            Literal(StringLiteral(_)) => self.ty.push_complete(Vector(Box::new(Scalar(I8)))),
+
+            BinOp { kind: op, ref mut left, ref mut right } => {
+                // First, sync the left and right types into the elem_type.
+                let ref mut elem_type = Unknown;
+                elem_type.push(&left.ty)?;
+                elem_type.push(&right.ty)?;
+
+                if !op.is_comparison() {
+                    elem_type.push(&self.ty)?;
+                }
+
+                // Now, attempt to push the elem_type back into left and right to "sync" them.
+                let mut changed = left.ty.push(elem_type)?;
+                changed |= right.ty.push(elem_type)?;
+
+                // For comparisons, force the type to be Bool.
+                if op.is_comparison() {
+                    if let Simd(_) = left.ty {
+                        changed |= self.ty.push(&Simd(Bool))?;
+                    } else {
+                        changed |= self.ty.push(&Scalar(Bool))?;
+                    }
+                } else {
+                    changed |= self.ty.push(elem_type)?;
+                }
+                Ok(changed)
+            }
+
+            UnaryOp { ref value, .. } => {
+                match value.ty {
+                    Scalar(ref kind) if kind.is_float() => {
+                        self.ty.push(&value.ty)
+                    }
+                    _ => {
+                        compile_err!("TODO")
+                    }
+                }
+            }
+
+            Cast { kind, .. } => {
+                self.ty.push_complete(Scalar(kind))
+            }
+
+            ToVec { ref mut child_expr } => {
+                // The base type is vec[{?,?}] - infer the key and value type.
+                let ref base_type = Vector(Box::new(Struct(vec![Unknown, Unknown])));
+                let mut changed = self.ty.push(base_type)?;
+
+                let mut set_types = false;
+                if let Dict(ref key, ref value) = child_expr.ty {
+                    if let Vector(ref mut elem) = self.ty {
+                        if let Struct(ref mut fields) = **elem {
+                            let pair_type = vec![key.clone(), value.clone()];
+                            for (ref mut field, ref child) in fields.iter_mut().zip(pair_type) {
+                                changed |= field.push(child)?;
+                            }
+                            set_types = true;
+                        }
+                    }
+                }
+
+                if !set_types {
+                    compile_err!("TODO")
+                } else {
+                    Ok(changed)
+                }
+            }
+
+            Ident(ref symbol) => {
+                if let Some(ref ty) = env.get(symbol) {
+                    self.ty.push(ty)
+                } else {
+                    compile_err!("TODO")
+                }
+            }
+
+            Negate(ref c) => {
+                self.ty.push(&c.ty)
+            }
+
+            Broadcast(ref c) => {
+                if let Scalar(ref kind) = c.ty {
+                    self.ty.push(&Simd(kind.clone()))
+                } else {
+                    compile_err!("TODO")
+                }
+            }
+
+            CUDF { ref return_ty, .. } => {
+                self.ty.push(return_ty)
+            }
+
+            Serialize(_) => {
+                let serialized_type = Vector(Box::new(Scalar(I8)));
+                self.ty.push_complete(serialized_type)
+            }
+
+            Deserialize{ ref value_ty, .. } => {
+                self.ty.push(value_ty)
+            }
+
+            Let { ref mut body, .. } => {
+                self.ty.sync(&mut body.ty)
+            }
+
+            MakeVector { ref mut elems } => {
+                // Sync the types of each element.
+                let mut elem_type = Unknown;
+                for e in elems.iter() {
+                    elem_type.push(&e.ty)?;
+                }
+
+                let mut changed = false;
+                for e in elems.iter_mut() {
+                    changed |= e.ty.push(&elem_type)?;
+                }
+
+                // If no error eccord, push the element type to the expression.
+                let ref vec_type = Vector(Box::new(elem_type));
+                changed |= self.ty.push(vec_type)?;
+                Ok(changed)
+            }
+
+            Zip { ref mut vectors } => {
+                let mut changed = false;
+
+                let ref base_type = Vector(Box::new(Struct(vec![Unknown; vectors.len()])));
+                changed |= self.ty.push(base_type)?;
+
+                // Push the vector type to each vector.
+                let mut types = vec![];
+                let mut set_types = false;
+                if let Vector(ref mut elem_type) = self.ty {
+                    if let Struct(ref mut vec_types) = **elem_type {
+                        for (vec_ty, vec_expr) in vec_types.iter_mut().zip(vectors.iter_mut()) {
+                            if let Vector(ref elem_type) = vec_expr.ty {
+                                changed |= vec_ty.push(elem_type)?;
+                            }
+                            types.push(vec_ty.clone());
+                        }
+                        set_types = true;
+                    }
+                }
+
+                if !set_types || types.len() != vectors.len() {
+                    compile_err!("TODO")
+                } else {
+                    let ref base_type = Vector(Box::new(Struct(types)));
+                    changed |= self.ty.push(base_type)?;
+                    Ok(changed)
+                }
+            }
+
+        MakeStruct { ref mut elems } => {
+            let mut changed = false;
+            let ref base_type = Struct(vec![Unknown; elems.len()]);
+            changed |= self.ty.push(base_type)?;
+
+            if let Struct(ref mut elem_types) = self.ty {
+                // Sync the type of each element and the expression struct type.
+                for (elem_ty, elem_expr) in elem_types.iter_mut().zip(elems.iter_mut()) {
+                    changed |= elem_ty.sync(&mut elem_expr.ty)?;
+                }
+                Ok(changed)
+            } else {
+                compile_err!("TODO")
+            }
+        }
+
+        GetField { expr: ref mut param, index } => {
+            if let Struct(ref mut elem_types) = param.ty {
+                let index = index as usize;
+                if index >= elem_types.len() {
+                    compile_err!("IndexError")
+                } else {
+                    self.ty.sync(&mut elem_types[index])
+                }
+            } else {
+                compile_err!("TODO")
+            }
+        }
+
+        /*
+        Length { ref mut data } => {
+            match data.ty {
+                Vector(_) => (),
+                Unknown => (),
+                _ => return compile_err!("Internal error: Length called on non-vector"),
+            }
+            push_complete_type(&mut expr.ty, Scalar(I64), "Length")
+        }
+
+        Slice {
+            ref mut data,
+            ref mut index,
+            ref mut size,
+        } => {
+            if let Vector(_) = data.ty {
+                let mut changed = false;
+                changed |= try!(push_complete_type(&mut index.ty, Scalar(I64), "Slice"));
+                changed |= try!(push_complete_type(&mut size.ty, Scalar(I64), "Slice"));
+                changed |= try!(push_type(&mut expr.ty, &data.ty, "Slice"));
+                Ok(changed)
+            } else {
+                compile_err!("Internal error: Slice called on {:?}, must be called on vector",
+                          data.ty)
+            }
+        }
+
+        Sort {
+            ref mut data,
+            ref mut keyfunc,
+        } => {
+            if let Vector(ref elem_type) = data.ty {
+                let mut changed = false;
+                if let Function(ref mut params, _) = keyfunc.ty {
+                    if params.len() == 1 {
+                        changed |= try!(push_type(&mut params[0], &elem_type, "Sort key return"));
+                    } else {
+                        return compile_err!("Internal error: Sort key has too many parameters");
+                    }
+                }
+                changed |= try!(push_type(&mut expr.ty, &data.ty, "Sort"));
+                Ok(changed)
+            } else {
+                compile_err!("Internal error: Sort called on {:?}, must be called on vector",
+                          data.ty)
+            }
+        }
+
+        Lookup {
+            ref mut data,
+            ref mut index,
+        } => {
+            if let Vector(ref elem_type) = data.ty {
+                let mut changed = false;
+                changed |= try!(push_complete_type(&mut index.ty, Scalar(I64), "Lookup"));
+                changed |= try!(push_type(&mut expr.ty, &elem_type, "Lookup"));
+                Ok(changed)
+            } else if let Dict(ref key_type, ref value_type) = data.ty {
+                let mut changed = false;
+                changed |= try!(push_type(&mut index.ty, &key_type, "Lookup"));
+                changed |= try!(push_type(&mut expr.ty, &value_type, "Lookup"));
+                Ok(changed)
+            } else if data.ty == Unknown {
+                Ok(false)
+            } else {
+                compile_err!("Internal error: Lookup called on {:?}", data.ty)
+            }
+        }
+
+        KeyExists {
+            ref mut data,
+            ref mut key,
+        } => {
+            if let Dict(ref key_type, _) = data.ty {
+                let mut changed = false;
+                changed |= try!(push_type(&mut key.ty, &key_type, "KeyExists"));
+                changed |= try!(push_complete_type(&mut expr.ty, Scalar(Bool), "KeyExists"));
+                Ok(changed)
+            } else if data.ty == Unknown {
+                Ok(false)
+            } else {
+                compile_err!("Internal error: KeyExists called on {:?}", data.ty)
+            }
+        }
+
+        Lambda {
+            ref mut params,
+            ref mut body,
+        } => {
+            let mut changed = false;
+
+            let base_type = Function(vec![Unknown; params.len()], Box::new(Unknown));
+            changed |= try!(push_type(&mut expr.ty, &base_type, "Lambda"));
+
+            if let Function(ref mut param_types, ref mut res_type) = expr.ty {
+                changed |= try!(sync_types(&mut body.ty, res_type, "Lambda return"));
+                for (param_ty, param_expr) in param_types.iter_mut().zip(params.iter_mut()) {
+                    changed |= try!(sync_types(param_ty, &mut param_expr.ty, "Lambda parameter"));
+                }
+            } else {
+                return compile_err!("Internal error: type of Lambda was not Function");
+            }
+
+            Ok(changed)
+        }
+        */
+
+            _ => unimplemented!()
+        }
     }
 }
