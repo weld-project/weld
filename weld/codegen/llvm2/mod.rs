31b112cde480cbf14ab60e3debcff0caaa2ebd80
Shoumik Palkar <shoumik@cs.stanford.edu>
2018-06-12 16:27:25 -0700

    more work on new codegen

--- weld/codegen/llvm2/mod.rs
+++ weld/codegen/llvm2/mod.rs
@@ -33,8 +33,6 @@ macro_rules! c_str {
     );
 }
 
-type SymbolMap = FnvHashMap<Symbol, LLVMValueRef>;
-
 pub struct CompiledModule;
 
 // XXX Why is this here...
@@ -107,27 +105,65 @@ pub fn compile_program(program: &Program,
     Ok(CompiledModule)
 }
 
+struct BasicBlockContext {
+    block: LLVMBasicBlockRef,
+    generated: bool,
+}
+
+impl BasicBlockContext {
+    fn new(block: LLVMBasicBlockRef, generated: bool) -> BasicBlockContext {
+        BasicBlockContext {
+            block: block,
+            generated: generated,
+        }
+    }
+}
+
+struct FunctionContext<'a> {
+    sir_program: &'a SirProgram,
+    sir_function: &'a SirFunction,
+    llvm_function: LLVMValueRef,
+    builder: LLVMBuilderRef,
+    symbols: FnvHashMap<Symbol, LLVMValueRef>,
+    blocks: FnvHashMap<BasicBlockId, BasicBlockContext>,
+}
+
+impl<'a> FunctionContext<'a> {
+    pub fn new(sir_program: &'a SirProgram,
+               sir_function: &'a SirFunction,
+               llvm_context: LLVMContextRef,
+               llvm_function: LLVMValueRef) -> FunctionContext<'a> {
+        FunctionContext {
+            sir_program: sir_program,
+            sir_function: sir_function,
+            llvm_function: llvm_function,
+            builder: unsafe { LLVMCreateBuilderInContext(llvm_context) },
+            symbols: FnvHashMap::default(),
+            blocks: FnvHashMap::default(),
+        }
+    }
+}
+
+impl<'a> Drop for FunctionContext<'a> {
+    fn drop(&mut self) {
+        unsafe { LLVMDisposeBuilder(self.builder); }
+    }
+}
+
 struct LlvmGenerator {
     conf: ParsedConf,
     context: LLVMContextRef,
     module: LLVMModuleRef,
-    builder: LLVMBuilderRef,
 }
 
 impl LlvmGenerator {
     fn new(conf: ParsedConf) -> LlvmGenerator {
-        let (context, module, builder) = unsafe {
-            let context = LLVMContextCreate();
-            let module = LLVMModuleCreateWithName(c_str!("main"));
-            let builder =  LLVMCreateBuilderInContext(context);
-            (context, module, builder)
-        };
-
-        LlvmGenerator {
-            conf: conf,
-            context: context,
-            module: module,
-            builder: builder,
+        unsafe {
+            LlvmGenerator {
+                conf: conf,
+                context: LLVMContextCreate(),
+                module: LLVMModuleCreateWithName(c_str!("main")),
+            }
         }
     }
 
@@ -157,82 +193,93 @@ impl LlvmGenerator {
     }
 
     unsafe fn generate_function(&mut self, program: &SirProgram, func: &SirFunction) -> WeldResult<()> {
-        // Maps a symbol name in a function to the function's LLVM values.
-        let ref mut symbol_map = SymbolMap::default();
         let mut argument_types = vec![];
         for (_, ty) in func.params.iter() {
             argument_types.push(self.llvm_type(ty)?);
         }
+
         let return_type = LLVMVoidTypeInContext(self.context);
         let function_type = LLVMFunctionType(return_type, argument_types.as_mut_ptr(), argument_types.len() as u32, 0);
-
-        // TODO - Need a name generator for functions!
         let function_name = CString::new(format!("f{}", func.id)).unwrap();
         let function = LLVMAddFunction(self.module, function_name.as_ptr(), function_type);
 
+        let ref mut context = FunctionContext::new(program, func, self.context, function);
+
         // Add the function parameters to the symbol map.
         for (i, (symbol, _)) in func.params.iter().enumerate() {
-            symbol_map.insert(symbol.clone(), LLVMGetParam(function, i as u32));
+            context.symbols.insert(symbol.clone(), LLVMGetParam(function, i as u32));
         }
 
         // Create the entry basic block, where we define alloca'd variables.
-        let entry_bb = LLVMAppendBasicBlockInContext(self.context, function, c_str!("entry"));
-        LLVMPositionBuilderAtEnd(self.builder, entry_bb);
+        let entry_bb = LLVMAppendBasicBlockInContext(self.context, context.llvm_function, c_str!("entry"));
+        LLVMPositionBuilderAtEnd(context.builder, entry_bb);
 
         // Generate local variables.
         for (symbol, ty) in func.locals.iter() {
             let name = CString::new(symbol.to_string()).unwrap();
-            let value = LLVMBuildAlloca(self.builder, self.llvm_type(ty)?, name.as_ptr()); 
-            symbol_map.insert(symbol.clone(), value);
+            let value = LLVMBuildAlloca(context.builder, self.llvm_type(ty)?, name.as_ptr()); 
+            context.symbols.insert(symbol.clone(), value);
+        }
+
+        // Generate the basic block references. We do this first so we can forward reference blocks
+        // if necessary.
+        for bb in func.blocks.iter() {
+            let name = CString::new(format!("b{}", bb.id)).unwrap();
+            let block = LLVMAppendBasicBlockInContext(self.context, context.llvm_function, name.as_ptr());
+            context.blocks.insert(bb.id, BasicBlockContext::new(block, false));
         }
         
         // Generate the first basic block.
-        let first_bb = self.generate_basic_block(program, func, &func.blocks[0], function, symbol_map)?;
+        let first_bb = self.generate_basic_block(program, context, &func.blocks[0])?;
 
         // Jump from locals to the first basic block.
-        LLVMPositionBuilderAtEnd(self.builder, entry_bb);
-        LLVMBuildBr(self.builder, first_bb);
-        LLVMPositionBuilderAtEnd(self.builder, first_bb);
+        LLVMPositionBuilderAtEnd(context.builder, entry_bb);
+        LLVMBuildBr(context.builder, first_bb);
+        LLVMPositionBuilderAtEnd(context.builder, first_bb);
 
-        // Finally, generate the remaining basic blocks.
+        // Finally, generate the remaining basic blocks. The basic blocks will handle returning
+        // from the function when necessary.
         for bb in func.blocks.iter().skip(1) {
-            let _ = self.generate_basic_block(program, func, bb, function, symbol_map)?;
+            let _ = self.generate_basic_block(program, context, bb)?;
         }
         Ok(())
     }
 
-    unsafe fn generate_basic_block(&mut self, program: &SirProgram,
-                                   func: &SirFunction,
-                                   bb: &BasicBlock,
-                                   function: LLVMValueRef,
-                                   symbol_map: &mut SymbolMap) -> WeldResult<LLVMBasicBlockRef> {
-        debug!("Generate basic block called");
-        let name = CString::new(format!("b{}", bb.id)).unwrap();
-        let block = LLVMAppendBasicBlockInContext(self.context, function, name.as_ptr());
-        LLVMPositionBuilderAtEnd(self.builder, block);
+    unsafe fn generate_basic_block(&mut self,
+                                   program: &SirProgram,
+                                   context: &mut FunctionContext,
+                                   bb: &BasicBlock) -> WeldResult<LLVMBasicBlockRef> {
 
+        let block = { 
+            let block_context = context.blocks.get_mut(&bb.id).unwrap();
+            if block_context.generated {
+                return Ok(block_context.block);
+            }
+            block_context.generated = true;
+            block_context.block
+        };
+
+        LLVMPositionBuilderAtEnd(context.builder, block);
         for statement in bb.statements.iter() {
-            self.generate_statement(program, func, statement, symbol_map)?;
+            self.generate_statement(program, context, statement)?;
         }
-        self.generate_terminator(program, func, &bb.terminator, symbol_map)?;
+        self.generate_terminator(program, context, &bb)?;
         Ok(block)
     }
 
     unsafe fn generate_statement(&mut self, program: &SirProgram,
-                                   func: &SirFunction,
-                                   statement: &Statement,
-                                   symbol_map: &mut SymbolMap) -> WeldResult<LLVMValueRef> {
+                                 context: &mut FunctionContext,
+                                 statement: &Statement) -> WeldResult<LLVMValueRef> {
         use sir::StatementKind::*;
-
         match statement.kind {
             AssignLiteral(ref value) => {
-                use ast::Type::{Scalar, Simd};
+                // use ast::Type::{Scalar, Simd};
                 use ast::LiteralKind::*;
                 let output = statement.output.as_ref().unwrap();
-                let pointer = *symbol_map.get(output).unwrap();
-                let output_type = func.symbol_type(output)?;
+                let pointer = *context.symbols.get(output).unwrap();
+                let output_type = context.sir_function.symbol_type(output)?;
                 let literal_type = self.llvm_type(output_type)?;
-                let constant = if let Simd(_) = output_type {
+                let constant = if let Type::Simd(_) = output_type {
                     unimplemented!()
                 } else {
                     match *value {
@@ -250,23 +297,35 @@ impl LlvmGenerator {
                        StringLiteral(_) => unimplemented!()
                     }
                 };
-                Ok(LLVMBuildStore(self.builder, constant, pointer)) 
+                Ok(LLVMBuildStore(context.builder, constant, pointer)) 
             }
             _ => unimplemented!(),
         }
     }
 
     unsafe fn generate_terminator(&mut self, program: &SirProgram,
-                                   func: &SirFunction,
-                                   terminator: &Terminator,
-                                   symbol_map: &mut SymbolMap) -> WeldResult<()> {
+                                   context: &mut FunctionContext,
+                                   bb: &BasicBlock) -> WeldResult<()> {
         use sir::Terminator::*;
-        match *terminator {
+        match bb.terminator {
             ProgramReturn(ref sym) => {
-                LLVMBuildRetVoid(self.builder);
+                LLVMBuildRetVoid(context.builder);
+            }
+            Branch { ref cond, ref on_true, ref on_false } => {
+                let cond_value = *context.symbols.get(cond).unwrap();
+
+                let on_true_block = context.blocks.get(&on_true).unwrap().block;
+                let on_false_block = context.blocks.get(&on_false).unwrap().block;
+                LLVMPositionBuilderAtEnd(context.builder, context.blocks.get(&bb.id).unwrap().block);
+                let _ = LLVMBuildCondBr(context.builder, cond_value, on_true_block, on_false_block);
+            }
+            JumpBlock(ref id) => {
+                let block = context.blocks.get(id).unwrap().block;
+                LLVMPositionBuilderAtEnd(context.builder, context.blocks.get(&bb.id).unwrap().block);
+                LLVMBuildBr(context.builder, block);
             }
             _ => unimplemented!(),
-        }
+        };
         Ok(())
     }
 }
@@ -274,7 +333,6 @@ impl LlvmGenerator {
 impl Drop for LlvmGenerator {
     fn drop(&mut self) {
         unsafe {
-            LLVMDisposeBuilder(self.builder);
             LLVMDisposeModule(self.module);
             LLVMContextDispose(self.context);
         }
