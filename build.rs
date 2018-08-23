f15c8568d266203348a693cd8ba7f2aa5d3e2d9a
Shoumik Palkar <shoumikpalkar@gmail.com>
2018-08-23 16:32:30 -0700

    Fix Incorrect Capacity Check in Dictionary (#384)

--- build.rs
+++ build.rs
@@ -1,41 +1,23 @@
-extern crate uuid;
-
 use std::env;
 use std::process::Command;
-use uuid::Uuid;
 
-use std::fs::File;
-use std::io::Write;
-use std::path::Path;
+/// Write the build ID into an environment variable.
+fn register_build_id() {
+    // Set the build ID, which is either unknown or the git hash
+    let unknown_build = String::from("unknown");
+    let build_id = match Command::new("git")
+        .arg("rev-parse")
+        .arg("--short")
+        .arg("HEAD")
+        .output() {
+            Ok(output) => String::from_utf8(output.stdout).unwrap_or(unknown_build),
+            Err(_) => unknown_build,
+        };
+    println!("cargo:rustc-env=BUILD_ID={}", build_id);
+}
 
-fn main() {
-    let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
-    // For writing a UUID for this build.
-    let out_dir = env::var("OUT_DIR").unwrap();
-    // Create a new MD5 hash for this build.
-
-    let dest_path = Path::new(&out_dir).join("build_uuid");
-    let mut f = File::create(&dest_path).unwrap();
-
-    let uuid = Uuid::new_v4();
-    let as_string = format!("{}", uuid);
-    f.write_all(as_string.as_bytes()).unwrap();
-
-    let status = Command::new("make")
-        .arg("-C")
-        .arg(format!("{}/weld_rt/cpp/st", project_dir))
-        .status()
-        .unwrap();
-    assert!(status.success());
-
-    let status = Command::new("make")
-        .arg("-C")
-        .arg(format!("{}/llvmext/", project_dir))
-        .status()
-        .unwrap();
-    assert!(status.success());
-
+/// Link the C++ libraries.
+fn link_stdcpp() {
     // Link C++ standard library and some Mac-specific libraries
     let target = env::var("TARGET").unwrap();
     if target == "x86_64-apple-darwin" {
@@ -45,12 +27,45 @@ fn main() {
         }
     }
     println!("cargo:rustc-link-lib=dylib=stdc++");
+}
 
-    // Link the dictst C++ library
+
+/// Build the single threaded dictionary.
+fn build_dictionary(project_dir: &str) {
+    let status = Command::new("make")
+        .arg("-C")
+        .arg(format!("{}/weld_rt/cpp/st", project_dir))
+        .status()
+        .unwrap();
+    assert!(status.success());
+
     println!("cargo:rustc-link-lib=static=dictst");
     println!("cargo:rustc-link-search=native={}/weld_rt/cpp/st", project_dir);
+}
 
-    // Link the LLVM extensions library
+/// Build the LLVM Extensions.
+fn build_llvmext(project_dir: &str) {
+    let status = Command::new("make")
+        .arg("-C")
+        .arg(format!("{}/llvmext/", project_dir))
+        .status()
+        .unwrap();
+    assert!(status.success());
+
     println!("cargo:rustc-link-lib=static=llvmext");
     println!("cargo:rustc-link-search=native={}/llvmext", project_dir);
+}
+
+fn main() {
+    let ref project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    // Buildd ID
+    register_build_id();
+
+    // Link stdlibc++
+    link_stdcpp();
+
+    // Build and link external libs.
+    build_dictionary(project_dir);
+    build_llvmext(project_dir);
 }
