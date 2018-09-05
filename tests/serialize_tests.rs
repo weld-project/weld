7e28fce2ba74a4f1520af2ee286b14afb84ae42e
Shoumik Palkar <shoumik.palkar@databricks.com>
2018-09-05 16:37:18 -0700

    Test for serde with with nested pointer dictionary

--- tests/serialize_tests.rs
+++ tests/serialize_tests.rs
@@ -144,3 +144,69 @@ fn dict_pointers() {
 
     assert_eq!(to_native(&expect), to_native(&result));
 }
+
+/// Tests dictionaries where the value contains nested pointers.
+#[test]
+fn dict_nested_pointers() {
+    let code = "|v: vec[i32]|
+        let dict2 = result(for(v, groupmerger[i32,vec[i32]], |b,i,e| merge(b, {e,[e,e,e,e,e]})));
+        tovec(deserialize[dict[i32,vec[vec[i32]]]](serialize(dict2)))";
+    let input_vec: Vec<i32> = (10..20).collect();
+
+    let dict2_inners: Vec<(i32, Vec<Vec<i32>>)> = input_vec.iter()
+                           .map(|e| (*e, vec![vec![*e; 5]])).collect();
+
+
+    // Need this to keep the reference to the inner vectors live.
+    let mut inner_refs = vec![];
+
+    // Construct the Key/Value vector.
+    let expect_vec = dict2_inners.iter()
+        .map(|e| {
+            let e1 = e.0;
+            let inner_vec = e.1.iter()
+                                   .map(|v| WeldVec::from(v))
+                                   .collect::<Vec<_>>();
+            let e2 = WeldVec::from(&inner_vec);
+
+            // Prevent the inner vector from being dropped by holding a ref to it.
+            inner_refs.push(inner_vec);
+
+            Pair::new(e1, e2)
+        })
+    .collect::<Vec<_>>();
+    let expect = WeldVec::from(&expect_vec);
+
+    let ref mut conf = default_conf();
+    let ref input_data = WeldVec::from(&input_vec);
+
+    let ret_value = compile_and_run(code, conf, input_data);
+    let data = ret_value.data() as *const WeldVec<Pair<i32, WeldVec<WeldVec<i32>>>>;
+    let result = unsafe { (*data).clone() };
+
+    fn to_native(v: &WeldVec<Pair<i32, WeldVec<WeldVec<i32>>>>) -> Vec<(i32, Vec<Vec<i32>>)> {
+        let mut res: Vec<(i32, Vec<Vec<i32>>)> = (0..v.len)
+            .into_iter()
+            .map(|x| {
+                let key = unsafe { (*v.data.offset(x as isize)).ele1 };
+                let val = unsafe { ((*v.data.offset(x as isize)).ele2).clone() };
+                let vec: Vec<Vec<i32>> = (0..val.len)
+                    .into_iter()
+                    .map(|y| unsafe {
+                        let inner_vec = (*val.data.offset(y as isize)).clone() ;
+                        (0..inner_vec.len).into_iter()
+                            .map(|z| *inner_vec.data.offset(z as isize) )
+                            .collect()
+                    })
+                    .collect();
+                (key, vec)
+            })
+        .collect();
+
+        // For dictionary outputs, we need to ignore the order.
+        res.sort_by_key(|a| a.0);
+        res
+    }
+
+    assert_eq!(to_native(&expect), to_native(&result));
+}
