ff367494ca8273efa1c2e20a731fad43cfce7582
Shoumik Palkar <shoumik.palkar@databricks.com>
2018-09-11 15:28:26 -0700

    More dictionary

--- weld/codegen/llvm2/dict_new.rs
+++ weld/codegen/llvm2/dict_new.rs
@@ -26,18 +26,19 @@ const VALUE_INDEX: u32 = KEY_INDEX + 1;
 /// Slot index of whether a slot is filled.
 const FILLED_INDEX: u32 = VALUE_INDEX + 1;
 
-// Dictionary Layout: { data*, capacity, size }
+// Dictionary Layout: { slot_array*, capacity, size }
 
 /// Data index.
-const DATA_INDEX: u32 = 0;
+const SLOT_ARR_INDEX: u32 = 0;
 /// Capacity index.
-const CAP_INDEX: u32 = DATA_INDEX + 1;
+const CAP_INDEX: u32 = SLOT_ARR_INDEX + 1;
 /// Size index.
 const SIZE_INDEX: u32 = CAP_INDEX + 1;
 
 pub struct Dict {
     pub name: String,
     pub dict_ty: LLVMTypeRef,
+    dict_inner_ty: LLVMTypeRef,
     pub key_ty: LLVMTypeRef,
     pub val_ty: LLVMTypeRef,
     key_comparator: LLVMValueRef,
@@ -46,9 +47,9 @@ pub struct Dict {
     module: LLVMModuleRef,
     slot_for_key: Option<LLVMValueRef>,
     new: Option<LLVMValueRef>,
-    get: Option<LLVMValueRef>,
+    lookup: Option<LLVMValueRef>,
     upsert: Option<LLVMValueRef>,
-    get_slot: Option<LLVMValueRef>,
+    resize: Option<LLVMValueRef>,
     key_exists: Option<LLVMValueRef>,
     size: Option<LLVMValueRef>,
     to_vec: Option<LLVMValueRef>,
@@ -67,7 +68,7 @@ impl CodeGenExt for Dict {
 }
 
 /// A dictionary slot.
-struct SlotType {
+pub struct SlotType {
     /// The type of the slot.
     slot_ty: LLVMTypeRef,
     /// The key type.
@@ -125,21 +126,21 @@ impl SlotType {
     /// Return the key pointer for the provided slot.
     ///
     /// The slot should be a pointer.
-    unsafe fn key(&mut self, builder: LLVMBuilderRef, value: LLVMValueRef) -> LLVMValueRef {
+    pub unsafe fn key(&mut self, builder: LLVMBuilderRef, value: LLVMValueRef) -> LLVMValueRef {
         LLVMBuildStructGEP(builder, value, KEY_INDEX, c_str!(""))
     }
 
     /// Return the value pointer for the provide slot.
     ///
     /// The slot should be a pointer.
-    unsafe fn value(&mut self, builder: LLVMBuilderRef, value: LLVMValueRef) -> LLVMValueRef {
+    pub unsafe fn value(&mut self, builder: LLVMBuilderRef, value: LLVMValueRef) -> LLVMValueRef {
         LLVMBuildStructGEP(builder, value, VALUE_INDEX, c_str!(""))
     }
 
     /// Return whether the provided slot is filled as an `i1`.
     ///
     /// The slot should be a pointer.
-    unsafe fn filled(&mut self, builder: LLVMBuilderRef, value: LLVMValueRef) -> LLVMValueRef {
+    pub unsafe fn filled(&mut self, builder: LLVMBuilderRef, value: LLVMValueRef) -> LLVMValueRef {
         let filled_pointer = LLVMBuildStructGEP(builder, value, FILLED_INDEX, c_str!(""));
         let filled = self.load(builder, filled_pointer).unwrap();
         LLVMBuildICmp(builder, LLVMIntNE, filled, self.i8(0), c_str!(""))
@@ -167,18 +168,19 @@ impl Dict {
             LLVMInt64TypeInContext(context)
         ];
 
-        debug_assert!(LLVMGetTypeKind(layout[DATA_INDEX as usize]) == LLVMTypeKind::LLVMPointerTypeKind);
+        debug_assert!(LLVMGetTypeKind(layout[SLOT_ARR_INDEX as usize]) == LLVMTypeKind::LLVMPointerTypeKind);
         debug_assert!(LLVMGetTypeKind(layout[CAP_INDEX as usize]) == LLVMTypeKind::LLVMIntegerTypeKind);
         debug_assert!(LLVMGetTypeKind(layout[SIZE_INDEX as usize]) == LLVMTypeKind::LLVMIntegerTypeKind);
 
-        let dict_ty = LLVMStructCreateNamed(context, c_name.as_ptr());
-        LLVMStructSetBody(dict_ty, layout.as_mut_ptr(), layout.len() as u32, 0);
+        let dict_inner_ty = LLVMStructCreateNamed(context, c_name.as_ptr());
+        LLVMStructSetBody(dict_inner_ty, layout.as_mut_ptr(), layout.len() as u32, 0);
 
         let name = c_name.into_string().unwrap();
 
         Dict {
             name: name,
-            dict_ty: dict_ty,
+            dict_ty: LLVMPointerType(dict_inner_ty, 0),
+            dict_inner_ty: dict_inner_ty,
             key_ty: key_ty,
             key_comparator: key_comparator,
             val_ty: val_ty,
@@ -187,15 +189,27 @@ impl Dict {
             module: module,
             slot_for_key: None,
             new: None,
-            get: None,
+            lookup: None,
             upsert: None,
-            get_slot: None,
+            resize: None,
             key_exists: None,
             size: None,
             to_vec: None,
             free: None,
             serialize: None,
         }
+    }
+
+    unsafe fn slot_array(&mut self, builder: LLVMBuilderRef, value: LLVMValueRef) -> LLVMValueRef {
+        self.load(builder, LLVMBuildStructGEP(builder, value, SLOT_ARR_INDEX, c_str!(""))).unwrap()
+    }
+
+    unsafe fn capacity(&mut self, builder: LLVMBuilderRef, value: LLVMValueRef) -> LLVMValueRef {
+        self.load(builder, LLVMBuildStructGEP(builder, value, CAP_INDEX, c_str!(""))).unwrap()
+    }
+
+    unsafe fn size(&mut self, builder: LLVMBuilderRef, value: LLVMValueRef) -> LLVMValueRef {
+        self.load(builder, LLVMBuildStructGEP(builder, value, SIZE_INDEX, c_str!(""))).unwrap()
     }
 
     /// Returns the probe start index for a hash.
@@ -220,17 +234,135 @@ impl Dict {
                       args.as_mut_ptr(), args.len() as u32, c_str!(""))
     }
 
+    /// Returns the slot pointer at a given index into the slot array.
     unsafe fn slot_at_index(&mut self,
                             builder: LLVMBuilderRef,
-                            data: LLVMValueRef,
+                            slot_array: LLVMValueRef,
                             index: LLVMValueRef) -> LLVMValueRef {
-        LLVMBuildGEP(builder, data, [index].as_mut_ptr(), 1, c_str!(""))
+        LLVMBuildGEP(builder, slot_array, [index].as_mut_ptr(), 1, c_str!(""))
     }
 
-    /// Return the slot for a given key.
+    /// Returns an `i1` determining whether the dictionary should be resized. 
+    unsafe fn should_resize(&mut self,
+                            builder: LLVMBuilderRef,
+                            size: LLVMValueRef,
+                            capacity: LLVMValueRef) -> LLVMValueRef {
+        let size = LLVMBuildNSWMul(builder, size, self.i64(10), c_str!(""));
+        let capacity = LLVMBuildNSWMul(builder, capacity, self.i64(MAX_LOAD_FACTOR), c_str!(""));
+        LLVMBuildICmp(builder, LLVMIntSGE, size, capacity)
+    }
+
+    unsafe fn gen_new_dict_with_capacity(&mut self,
+                                         builder: LLVMBuilderRef,
+                                         intrinsics: &Intrisics,
+                                         capacity: LLVMValueRef,
+                                         run: LLVMValueRef) -> LLVMValueRef {
+
+        let alloc_size = LLVMBuildNSWMul(builder, capacity, self.size_of(self.slot_ty.slot_ty));
+        let bytes = intrinsics.call_weld_run_malloc(builder, run, alloc_size, None);
+        let slot_array = LLVMBuildBitCast(builder, bytes, LLVMPointerType(self.slot_ty.slot_ty, 0));
+
+        let mut dict = LLVMBuildInsertValue(
+            builder, LLVMGetUndef(self.dict_inner_ty), slot_array, SLOT_ARR_INDEX, c_str!(""));
+        dict = LLVMBuildInsertValue(builder, dict, self.i64(capacity), CAP_INDEX, c_str!(""));
+        LLVMBuildInsertValue(builder, dict, self.i64(0), SIZE_INDEX, c_str!(""))
+    }
+
+    /// Resize the dictionary if necessary.
+    ///
+    /// Returns an `i1` indicating whether a resize occurred. If the dictionary was resized, slots
+    /// acquired from the dictionary should be re-acquired.
+    unsafe fn gen_resize(&mut self,
+                         builder: LLVMBuilderRef,
+                         intrinsics: &Intrinsics,
+                         dict: LLVMValueRef,
+                         run: LLVMValueRef) -> LLVMValueRef {
+
+        if self.resize.is_none() {
+            let mut arg_tys = [
+                self.dict_ty,
+                self.run_handle_type()
+            ];
+
+            let ret_ty = self.i1_type(),
+            let name = format!("{}.resize", self.name);
+
+            let (function, builder, entry_block) = self.define_function(ret_ty, &mut arg_tys, name);
+
+            let dict = LLVMGetParam(function, 0);
+            let run = LLVMGetParam(function, 1);
+
+            let size = self.size(builder, dict);
+            let capacity = self.capacity(builder, capacity);
+
+            // Resize
+            let resize_block = LLVMAppendBasicBlockInContext(self.context(), function,  c_str!(""));
+
+            // Copy loop.
+            let copy_top_block = LLVMAppendBasicBlockInContext(self.context(), function,  c_str!(""));
+            let copy_cpy_block = LLVMAppendBasicBlockInContext(self.context(), function,  c_str!(""));
+            let copy_bot_block = LLVMAppendBasicBlockInContext(self.context(), function,  c_str!(""));
+            let copy_fin_block = LLVMAppendBasicBlockInContext(self.context(), function,  c_str!(""));
+
+            // Exit
+            let exit_block = LLVMAppendBasicBlockInContext(self.context(), function,  c_str!(""));
+
+            let should_resize = self.should_resize(builder, size, capacity);
+            LLVMBuildCondBr(builder, should_resize, resize_block, exit_block);
+
+            LLVMPositionBuilderAtEnd(builder, resize_block);
+
+            // Resize the dictionary.
+            let new_capacity = LLVMBuildNSWMul(builder, capacity, self.i64(2), c_str!(""));
+            let new_dictionary = self.gen_new_dict_with_capacity(builder, intrinsics, new_capacity, run);
+
+            // Loop to copy slots:
+            //
+            //  br top
+            // top:
+            //  i = phi [ resizeblock, 0 ], [ bottom, i2 ]
+            //  slot = slotatindex(i)
+            //  br slot.filled ? copyblock : bottom
+            // cpy:
+            //  newslot = getempty(new_dictionary, slot.hash)
+            //  memcpy(slot, newslot, sizeof(slot))
+            //  br bottom
+            // bot:
+            //  i2 = i + 1
+            //  br i2 == capacity : end ? top
+            // fin:
+            //  free(dict.slot_array)
+            //  memcpy(dict, new_dictionary, sizeof(dict))
+            //  br exit
+            // exit:
+            //  ...
+
+            LLVMBuildBr(builder, exit_block);
+
+            LLVMPositionBuilderAtEnd(builder, exit_block);
+            let resized = LLVMBuildPhi(builder, self.i1_type(), c_str!(""));
+            LLVMBuildRet(builder, resized);
+
+            self.resize = Some(function);
+            LLVMDisposeBuilder(builder);
+        }
+
+        let mut args = [dict, run];
+        LLVMBuildCall(builder,
+                         self.slot_for_key.unwrap(),
+                         args.as_mut_ptr(),
+                         args.len() as u32, c_str!(""))
+
+    }
+
+    /// Return the slot for a given key. The returned slot may be uninitialized if the key does not
+    /// exist in the dictionary.
+    ///
+    /// The current algorithm assumes a power-of-two hash table and performs standard linear
+    /// probing.
     unsafe fn gen_slot_for_key(&mut self,
                                builder: LLVMBuilderRef,
-                               data: LLVMValueRef,
+                               slot_array: LLVMValueRef,
                                capacity: LLVMValueRef,
                                hash: LLVMValueRef,
                                key: LLVMValueRef) -> LLVMValueRef {
@@ -239,7 +371,7 @@ impl Dict {
         //
         // Generated Code:
         //
-        // define probe(dict.slot *data, i64 capacity, i32 hash, key *key) -> slot {
+        // define probe(dict.slot *slot_array, i64 capacity, i32 hash, key *key) -> slot {
         //   start = getstart(hash, cap);
         //   jump top
         // top:
@@ -276,7 +408,7 @@ impl Dict {
 
             LLVMPositionBuilderAtEnd(builder, top_block);
             let index = LLVMBuildPhi(builder, self.i64_type(), c_str!(""));
-            let slot = self.slot_at_index(builder, data, index);
+            let slot = self.slot_at_index(builder, slot_array, index);
 
             let filled = self.slot_ty.filled(builder, slot);
             LLVMBuildCondBr(builder, filled, check_key_block, return_block);
@@ -302,50 +434,133 @@ impl Dict {
             LLVMDisposeBuilder(builder);
         }
 
-        let mut args = [data, capacity, hash, key];
+        let mut args = [slot_array, capacity, hash, key];
         LLVMBuildCall(builder,
                          self.slot_for_key.unwrap(),
                          args.as_mut_ptr(),
                          args.len() as u32, c_str!(""))
     }
 
-    /*
+    /// Returns the pointer to the slot for a key.
+    ///
+    /// If the key does not exist, the first uninitialized slot is returned.
     pub unsafe fn gen_upsert(&mut self,
-                             builder: LLVMBuilderRef,
-                             run: LLVMValueRef,
-                             dict: LLVMValueRef,
-                             key: LLVMValueRef,
-                             hash: LLVMValueRef,
-                             val: LLVMValueRef) -> WeldResult<LLVMValueRef> {
+                         builder: LLVMBuilderRef,
+                         intrinsics: &Intrinsics,
+                         dict: LLVMValueRef,
+                         key: LLVMValueRef,
+                         hash: LLVMValueRef,
+                         default: LLVMValueRef,
+                         run: LLVMValueRef) -> WeldResult<LLVMValueRef> {
 
-        // Type checking.
-        debug_assert!(LLVMGetTypeKind(dict) == LLVMTypeKind::LLVMPointerTypeKind);
-        debug_assert!(LLVMGetTypeKind(key) == LLVMTypeKind::LLVMPointerTypeKind);
-        debug_assert!(LLVMGetTypeKind(val) == LLVMTypeKind::LLVMPointerTypeKind);
-        debug_assert!(LLVMGetTypeKind(ihash) == LLVMTypeKind::LLVMIntegerTypeKind);
+        if self.lookup.is_none() {
+            let mut arg_tys = [
+                self.dict_ty,
+                LLVMPointerType(self.key_ty, 0),
+                self.hash_type(),
+                self.val_ty,
+                self.run_handle_type(),
+            ];
+            let ret_ty = LLVMPointerType(self.slot_ty.slot_ty, 0);
 
-        SlotType s = probe(capacity, hash, key);
-        if s.filled {
-            return s.value
-        } else {
-            s.filled = true;
-            s.key = key;
-            s.value = value;
-            if (resize()) {
-                s = probe(capacity, hash, key);
-            } 
-            return s.value
+            let name = format!("{}.upsert", self.name);
+            let (function, builder, entry_block) = self.define_function(ret_ty, &mut arg_tys, name);
+
+            let set_default_block = LLVMAppendBasicBlockInContext(self.context(), function, c_str!(""));
+            let reacquire_slot_block = LLVMAppendBasicBlockInContext(self.context(), function, c_str!(""));
+            let return_block = LLVMAppendBasicBlockInContext(self.context(), function, c_str!(""));
+
+            let dict = LLVMGetParam(function, 0);
+            let key = LLVMGetParam(function, 1);
+            let hash = LLVMGetParam(function, 2);
+            let default = LLVMGetParam(function, 3);
+            let run = LLVMGetParam(function, 4);
+
+            let slot_array = self.slot_array(builder, dict);
+            let capacity = self.capacity(builder, dict);
+            let slot = self.gen_slot_for_key(builder, slot_array, capacity, hash, key);
+
+            let filled = self.slot_ty.filled(builder, slot);
+            LLVMBuildCondBr(filled, return_block, set_default_block);
+
+            LLVMPositionBuilderAtEnd(builder, set_default_block);
+
+            self.slot_ty.init(builder, slot, key, default);
+            let size_pointer = LLVMBuildStructGEP(builder, value, SIZE_INDEX, c_str!(""));
+            let size = self.load(size_pointer).unwrap();
+            let new_size = LLVMBuildNSWAdd(builder, size, self.i64(1), c_str!(""));
+            LLVMBuildStore(builder, size, size_pointer);
+
+            // Check for resize.
+            let resized = self.gen_resize(builder, intrinsics, dict, run);
+            LLVMBuildCondBr(resized, reacquire_slot_block, return_block);
+
+            LLVMPositionBuilderAtEnd(builder, reacquire_slot_block);
+            // Builder was resized - we need to reacquire the slot.
+            let resized_slot = self.gen_slot_for_key(builder, slot_array, capacity, hash, key);
+
+            LLVMPositionBuilderAtEnd(builder, return_block);
+
+            let return_slot = LLVMBuildPhi(builder, ret_ty, c_str!(""));
+            LLVMBuildRet(builder, return_slot);
+
+            // Set the PHI value.
+            let mut blocks = [entry_block, reacquire_slot_block];
+            let mut values = [slot, resized_slot];
+            LLVMAddIncoming(return_slot, values.as_mut_ptr(), blocks.as_mut_ptr(), values.len() as u32);
+
+            self.upsert = Some(function);
+            LLVMDisposeBuilder(builder);
         }
+
+        let mut args = [dict, key, hash, default, run];
+        return Ok(LLVMBuildCall(builder,
+                                self.upsert.unwrap(),
+                                args.as_mut_ptr(),
+                                args.len() as u32,
+                                c_str!("")))
     }
 
+    /// Returns the pointer to the slot for a key.
+    ///
+    /// If the key does not exist, the first uninitialized slot is returned.
     pub unsafe fn gen_lookup(&mut self,
-                         builder: LLVMValueRef,
-                         run: LLVMValueRef,
+                         builder: LLVMBuilderRef,
+                         dict: LLVMValueRef,
                          key: LLVMValueRef,
                          hash: LLVMValueRef) -> WeldResult<LLVMValueRef> {
-        SlotType s = probe(capacity, hash, key);
-        return s;
+
+        if self.lookup.is_none() {
+            let mut arg_tys = [
+                self.dict_ty,
+                LLVMPointerType(self.key_ty, 0),
+                self.hash_type(),
+            ];
+            let ret_ty = LLVMPointerType(self.slot_ty.slot_ty, 0);
+
+            let name = format!("{}.lookup", self.name);
+            let (function, builder, _) = self.define_function(ret_ty, &mut arg_tys, name);
+
+            let dict = LLVMGetParam(function, 0);
+            let key = LLVMGetParam(function, 1);
+            let hash = LLVMGetParam(function, 2);
+
+            let slot_array = self.slot_array(builder, dict);
+            let capacity = self.capacity(builder, dict);
+            let slot = self.gen_slot_for_key(builder, slot_array, capacity, hash, key);
+
+            LLVMBuildRet(builder, slot);
+
+            self.lookup = Some(function);
+            LLVMDisposeBuilder(builder);
+        }
+
+        let mut args = [dict, key, hash];
+        return Ok(LLVMBuildCall(builder,
+                                self.lookup.unwrap(),
+                                args.as_mut_ptr(),
+                                args.len() as u32,
+                                c_str!("")))
     }
-    */
 }
 
