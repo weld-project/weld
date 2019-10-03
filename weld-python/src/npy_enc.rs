2549982b585aec8cfc1024f91c915aac133829c0
Shoumik Palkar <shoumik@cs.stanford.edu>
2019-10-03 08:45:50 +0530

    dtype mapping

--- weld-python/src/npy_enc.rs
+++ weld-python/src/npy_enc.rs
@@ -14,11 +14,24 @@
 //! inefficient. Weld will eventually include a tensor[T,shape] type that will support this.
 
 use pyo3::prelude::*;
-use pyo3::import_exception;
+use pyo3::wrap_pyfunction;
 
 use numpy::PyArray1;
 
 use weld::data;
+
+#[pyclass]
+struct WeldVec {
+    vec: data::WeldVec<i32>,
+}
+
+impl WeldVec {
+    fn new(vec: data::WeldVec<i32>) -> Self {
+        WeldVec {
+            vec
+        }
+    }
+}
 
 /// Converts a 1D NumPy array into a `WeldVec` that can be passed to the Weld runtime.
 fn to_weld_1d<T>(array: &PyArray1<T>) -> data::WeldVec<T> {
@@ -34,7 +47,13 @@ fn to_numpy_1d<T>(vec: &data::WeldVec<T>) -> PyArray1<T> {
     unimplemented!("This needs to be implemented");
 }
 
+#[pyfunction]
+fn to_weld_1d_i32(array: &PyArray1<i32>) -> PyResult<WeldVec> {
+    Ok(WeldVec::new(to_weld_1d::<i32>(array)))
+}
+
 #[pymodule]
 fn numpy_encoders(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(to_weld_1d_i32)).unwrap();
     Ok(())
 }
--- weld-python/weld/encoders/numpy.py
+++ weld-python/weld/encoders/numpy.py
@@ -1,10 +1,77 @@
 """
-Implements some basic encoders for NumPy arrays.
+Implements encoders for NumPy values.
+
+Zero-copy conversions (in particular, to 1D arrays) are implemented here
+directly since they only involve a pointer copy. Conversions of types that
+currently require copies are implemented in Rust.
 """
 
 import numpy as np
 
 from .encoder_base import *
+from ..types import *
+
+# Maps a string dtype representation to a Weld scalar type.
+_known_types = {
+        'int8': I8(),
+        'int16': I16(),
+        'int32': I32(),
+        'int64': I64(),
+        'uint8': U8(),
+        'uint16': U16(),
+        'uint32': U32(),
+        'uint64': U64(),
+        'float': F32(),
+        'float32': F32(),
+        'double': F64(),
+        'float64': F64(),
+        'bool': Bool()
+        }
+
+def dtype_to_weld_type(ty):
+    """Converts a NumPy data type to a Weld type.
+
+    The data type can be a any type that can be converted to a NumPy dtype,
+    e.g., a string (e.g., 'int32') or a NumPy scalar type (e.g., np.int32). The
+    type chosen follows the rules specified by NumPy here:
+
+    https://docs.scipy.org/doc/numpy/reference/arrays.dtypes.html#dtype
+
+    For example, 'i8' will map to an int64 in Weld, since it indicates a signed
+    integer that has eight bytes.
+
+    Examples
+    --------
+    >>> dtype_to_weld_type('int32')
+    <weld.types.I32 object at 0x...>
+    >>> dtype_to_weld_type('float')
+    <weld.types.F64 object at 0x...>
+    >>> dtype_to_weld_type('i8')
+    <weld.types.I64 object at 0x...>
+    >>> dtype_to_weld_type(np.int16)
+    <weld.types.I16 object at 0x...>
+
+    Parameters
+    ----------
+    ty : str or dtype or NumPy scalar type
+        The NumPy type to convert
+
+    Returns
+    -------
+    WeldType
+
+    """
+    if not isinstance(ty, np.dtype):
+        ty = np.dtype(ty)
+
+    ty = str(ty)
+    if ty in _known_types:
+        return _known_types.get(ty)
+
+    if ty.startswith('S'):
+        raise TypeError("Python 2 strings not supported -- use Unicode")
+    if ty.find('U') != -1:
+        raise NotImplementedError("Unicode strings not yet supported")
 
 class NumPyWeldEncoder(WeldEncoder):
     pass
