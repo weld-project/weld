2b79b8188a08b728c726e6398a4654e462a17e9d
Shoumik Palkar <shoumik@cs.stanford.edu>
2020-02-07 15:17:05 -0800

    rewrite tree stuff for laziness

--- weld-python/tests/lazy_test.py
+++ weld-python/tests/lazy_test.py
@@ -6,19 +6,15 @@ from weld.encoders import PrimitiveWeldEncoder, PrimitiveWeldDecoder
 from weld.types import *
 from weld.lazy import *
 
-class LazyObject(WeldLazyMixin):
-    """ A thin wrapper around a lazy Weld value. """
-    pass
-
 def test_simple():
     a = 1
     b = 2
     comp1 = "a + b"
 
-    a = Dependency(1, I32(), PrimitiveWeldEncoder())
-    b = Dependency(2, I32(), PrimitiveWeldEncoder())
-    comp1 = WeldLazyMixin(
-            "{0} + {1}".format(a.expr_id, b.expr_id),
+    a = PhysicalValue(1, I32(), PrimitiveWeldEncoder())
+    b = PhysicalValue(2, I32(), PrimitiveWeldEncoder())
+    comp1 = WeldLazy(
+            "{0} + {1}".format(a.id, b.id),
             [a, b],
             I32(),
             PrimitiveWeldDecoder())
