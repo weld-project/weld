c7af8b7961172beec7ac6fb700548b0c8fc34199
Shoumik Palkar <shoumik@cs.stanford.edu>
2020-02-12 13:48:54 -0800

    travis updates

--- .travis/test_multi_version.sh
+++ .travis/test_multi_version.sh
@@ -3,50 +3,32 @@
 LLVM_VERSION=$1
 LLVM_SYS_VERSION=$2
 PYTHON_VERSION=$3
-VENV_HOME=`pwd`/.virtualenv
 
 # create llvm-config symlink
 sudo rm -f /usr/bin/llvm-config
 sudo ln -s /usr/bin/llvm-config-$LLVM_VERSION /usr/bin/llvm-config
 export WELD_HOME=`pwd`
 
-# source $VENV_HOME/python$PYTHON_VERSION/bin/activate
-cd python/pyweld
-# python setup.py install
-cd ../..
-
-cd python/grizzly
-# python setup.py install
-cd ../..
-
 # set llvm-sys crate version
 sed -i "s/llvm-sys = \".*\"/llvm-sys = \"$LLVM_SYS_VERSION\"/g" Cargo.toml
 
 # build and test
-# Note that cargo build must, counterintuitively, come after setup.py install,
-# because numpy_weld_convertor.cpp is built by cargo.
 cargo clippy
 cargo fmt -- --check
 cargo build #--release
 cargo test
 
-# XXX Disabling the Python tests for now, since they rely on some features this
-# backend does not support (e.g., vector comparisons and sorting). We'll
-# re-enable these once those features are plugged back in.
-
-# export LD_LIBRARY_PATH=`pwd`/target/release
-# python python/grizzly/tests/grizzly_test.py
-# python python/grizzly/tests/numpy_weld_test.py
-
-# run tests for nditer - first need to install weldnumpy
-# cd python/numpy
-# python setup.py install
-# python ../../examples/python/nditer/nditer_test.py
-# cd ../..
-
-# cd $WELD_HOME/weld-benchmarks; python run_benchmarks.py -b tpch_q1 tpch_q6 vector_sum map_reduce data_cleaning crime_index crime_index_simplified -n 5 -f results.tsv -v -d -p performance.png
-# mkdir -p $WELD_HOME/results
-# mv performance.png $WELD_HOME/results
-# mv results.tsv $WELD_HOME/results
+# Python Tests
 cd $WELD_HOME
-# deactivate
+virtualenv travis-test-env
+source travis-test-env/bin/activate
+cd weld-python
+pip install -r requirements.txt
+
+# Install Rust Requirements
+rustup toolchain install nightly
+rustup default nightly
+pip install -e .
+
+# Run the tests
+pytest
