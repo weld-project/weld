d3247d5205c3568b6b9a410480f484458c7b5b83
Shoumik Palkar <shoumik@cs.stanford.edu>
2020-02-12 18:08:31 -0800

    edits

--- .travis/test_multi_version.sh
+++ .travis/test_multi_version.sh
@@ -9,31 +9,18 @@ PYTHON_VERSION=$3
 # Weld Core Tests
 # ----------------------------------------------------------
 
-# create llvm-config symlink
-sudo rm -f /usr/bin/llvm-config
-sudo ln -s /usr/bin/llvm-config-$LLVM_VERSION /usr/bin/llvm-config
-export WELD_HOME=`pwd`
-
 # set llvm-sys crate version
 sed -i "s/llvm-sys = \".*\"/llvm-sys = \"$LLVM_SYS_VERSION\"/g" Cargo.toml
 
 # Python Tests
 # ----------------------------------------------------------
 
-cd $WELD_HOME
-python$PYTHON_VERSION -m venv travis-test-env
-source travis-test-env/bin/activate
-cd weld-python
-
-pip install numpy pandas pytest setuptools-rust
-
 # Install Rust Requirements
-rustup toolchain install nightly
 rustup default nightly
+pushd weld-python/
 pip install -e .
 
 # Run the tests
 pytest
 
-deactivate
-cd $WELD_HOME
+popd
